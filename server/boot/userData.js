d15455fc1c23af8812512ab345a95e339fa40994
ergot <wjose14@hotmail.com>
2017-04-16 21:15:49 +0200

    ajout de fixture user

---
+++ server/boot/userData.js
@@ -0,0 +1,22 @@
+'use strict';
+var async = require('async');
+module.exports = function(app) {
+  // 'name' of your mongo connector, you can find it in datasource.json
+
+  // WARNING: Calling this function deletes all data! Use autoupdate() to preserve data.
+  app.dataSources.mongoDs.automigrate('user', function(err) {
+    if (err) throw err;
+
+    app.models.user.create([{
+      email: 'admin@ifocop.com',
+      password: 'admin'
+    }, {
+      email: 'jose@ifocop.com',
+      password: 'jose'
+    }], function(err, users) {
+      if (err) throw err;
+      console.log('Models created: \n', users);
+    });
+  });
+
+};
