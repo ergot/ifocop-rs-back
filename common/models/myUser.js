2fda1ff933e4be5f55b41bd5b7e8e07c2e40ed06
ergot <wjose14@hotmail.com>
2017-04-23 16:56:06 +0200

    #1 ajout d un hook: envoie de l email + reponse à la requette http

--- common/models/myUser.js
+++ common/models/myUser.js
@@ -1,5 +1,38 @@
 'use strict';
+var config = require('../../server/config.json');
+var path = require('path');
 
-module.exports = function(User) {
+module.exports = function(user) {
+  //send verification email after registration
+  user.afterRemote('create', function(context, userInstance, next) {
+    console.log('> user.afterRemote triggered');
 
+    //configuration de l email envoyer à l enregistrement
+    var options = {
+      type: 'email',
+      to: userInstance.email,
+      from: 'noreply@loopback.com',
+      subject: 'Thanks for registering.',
+      // template de l email: avec un href /myUsers/confirm?iud=XXXX qui permet de valider le compte
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
+
+    //envoie de l email
+    userInstance.verify(options, function(err, response, next) {
+      if (err) return next(err);
+
+      console.log('> verification email sent:', response);
+
+      //reponse http apres l envoie de l email
+      context.res.render('response', {
+        title: 'Signed up successfully',
+        content: 'Please check your email and click on the verification link ' -
+        'before logging in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
 };
