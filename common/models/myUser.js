f4113307b0e82f4b554c57ed579e684e84a97afa
ergot <wjose14@hotmail.com>
2017-05-14 17:54:09 +0200

    #7 eslint

--- common/models/myUser.js
+++ common/models/myUser.js
@@ -3,11 +3,11 @@ var config = require('../../server/config.json');
 var path = require('path');
 
 module.exports = function (User) {
-  //send verification email after registration
+  // send verification email after registration
   User.afterRemote('create', function (context, userInstance, next) {
     console.log('> user.afterRemote triggered');
 
-    //configuration de l email envoyer à l enregistrement
+    // configuration de l email envoyer à l enregistrement
     var options = {
       type: 'email',
       to: userInstance.email,
@@ -16,18 +16,18 @@ module.exports = function (User) {
       // template de l email: avec un href /myUsers/confirm?iud=XXXX qui permet de valider le compte
       template: path.resolve(__dirname, '../../server/views/verify.ejs'),
       redirect: '/verified',
-      user: User
+      user: User,
     };
 
-    //envoie de l email
+    // envoie de l email
     userInstance.verify(options, function(err, response, next) {
       if (err) return next(err);
 
       console.log('> verification email sent:', response);
 
-      //reponse http apres l envoie de l email
+      // reponse http apres l envoie de l email
       context.res.sendStatus(200);
-      //si besoin creer le template server/views/response.ejs
+      // si besoin creer le template server/views/response.ejs
       // context.res.render('response', {
       //   title: 'Signed up successfully',
       //   content: 'Please check your email and click on the verification link ' -
@@ -40,7 +40,6 @@ module.exports = function (User) {
 
   // 3 send password reset link when requested
   User.on('resetPasswordRequest', function (info) {
-
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' +
       info.accessToken.id + '">here</a> to reset your password';
@@ -49,7 +48,7 @@ module.exports = function (User) {
       to: info.email,
       from: info.email,
       subject: 'Password reset',
-      html: html
+      html: html,
     }, function (err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
