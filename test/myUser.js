5218947130f3152007b6cbce152dfe18511762f6
ergot <wjose14@hotmail.com>
2017-05-16 19:58:15 +0200

    #7 controle que le membre voit que les users validée

--- test/myUser.js
+++ test/myUser.js
@@ -10,11 +10,11 @@ let adminToken = null;
 let userToken = null;
 const adminCompte = {
   'email': 'admin@yopmail.com',
-  'password': 'admin',
+  'password': 'password',
 };
 const userCompte = {
   email: 'jose@yopmail.com',
-  password: 'jose',
+  password: 'password',
 };
 
 describe('myUser', function() {
@@ -53,12 +53,14 @@ describe('myUser', function() {
           done();
         });
     });
-    it('user can get all users', function(done) {
+    it('member can get all users Verified', function (done) {
       chai.request(urlRoot)
         .get('/api/myUsers')
         .set('Authorization', userToken)
         .end((err, res) => {
-          console.log(res.body);
+          res.body.forEach((user) => {
+            expect(user.email).to.not.equal('unverified@yopmail.com');
+          });
           expect(res).to.have.status(200);
           expect(res.body[0].email).to.exist;
           done();
