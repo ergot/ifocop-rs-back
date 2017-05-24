e775120f15c9acd799146d22fbf38f301f820e1e
ergot <wjose14@hotmail.com>
2017-05-24 17:52:23 +0200

    #7 eslint

--- common/models/myUser.js
+++ common/models/myUser.js
@@ -3,9 +3,9 @@ var config = require('../../server/config.json');
 var path = require('path');
 const async = require('async');
 
-module.exports = function (User) {
+module.exports = function(User) {
   // send verification email after registration
-  User.afterRemote('create', function (context, userInstance, next) {
+  User.afterRemote('create', function(context, userInstance, next) {
     console.log('> user.afterRemote triggered');
 
     // configuration de l email envoyer à l enregistrement
@@ -40,7 +40,7 @@ module.exports = function (User) {
   });
 
   // 3 send password reset link when requested
-  User.on('resetPasswordRequest', function (info) {
+  User.on('resetPasswordRequest', function(info) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' +
       info.accessToken.id + '">here</a> to reset your password';
@@ -50,7 +50,7 @@ module.exports = function (User) {
       from: info.email,
       subject: 'Password reset',
       html: html,
-    }, function (err) {
+    }, function(err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
     });
@@ -69,12 +69,12 @@ module.exports = function (User) {
     }
 
 // eslint-disable-next-line max-len
-    RoleMapping.find({where: {principalId: accessToken}}, function (err, roleMappings) {
+    RoleMapping.find({where: {principalId: accessToken}}, function(err, roleMappings) {
       if (err) throw err;
       if (roleMappings.length > 1) throw 'User avec plusieurs roles non gérer';
-      Role.find({where: {id: roleMappings[0].roleId}}, function (err, roles) {
-          asyncDone(null, roles[0]);
-        }
+      Role.find({where: {id: roleMappings[0].roleId}}, function(err, roles) {
+        asyncDone(null, roles[0]);
+      }
       );
     });
   };
@@ -82,13 +82,13 @@ module.exports = function (User) {
    * Change le filtre ,pour un get myUser du role user, pour avoir que les
    * users avec un email valide
    */
-  User.beforeRemote('find', function (ctx, modelInstance, next) {
+  User.beforeRemote('find', function(ctx, modelInstance, next) {
     if (ctx.req.baseUrl === '/api/myUsers' & ctx.req.method === 'GET') {
       async.series([
-        function (asyncDone) {
+        function(asyncDone) {
           getRole(ctx.req.accessToken.userId, asyncDone);
         },
-      ], function (err, results) {
+      ], function(err, results) {
         if (results[0].name === 'member') {
           ctx.args.filter = {where: {emailVerified: true}};
         }
