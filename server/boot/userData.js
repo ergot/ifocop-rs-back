6203e2e34c85773fee4276276edbc7e8c3a59dba
ergot <wjose14@hotmail.com>
2017-05-25 10:50:43 +0200

    #7 refacto de la creation des users avec leur roles

--- server/boot/userData.js
+++ server/boot/userData.js
@@ -1,74 +1,44 @@
 'use strict';
+const userFixture = require('./userFixture');
 module.exports = function (app) {
   const Role = app.models.Role;
   const RoleMapping = app.models.RoleMapping;
   // 'name' of your mongo connector, you can find it in datasource.json
 
+  function setRole(roleName, users) {
+    Role.create({
+      name: roleName,
+    }, function (err, role) {
+      if (err) throw err;
+      const toto = (function () {
+        let data = [];
+        users.forEach((user) => {
+          data.push(
+            {
+              principalType: RoleMapping.USER,
+              principalId: user.id,
+            }
+          );
+        });
+        return data;
+      })();
+
+      role.principals.create(toto, function (err, principal) {
+        if (err) throw err;
+        console.log(principal);
+      });
+    });
+  }
+
   // WARNING: Calling this function deletes all data! Use autoupdate() to preserve data.
   app.dataSources.mongoDs.automigrate('myUser', function (err) {
     if (err) throw err;
 
-    app.models.myUser.create([{
-      email: 'admin@yopmail.com',
-      password: 'password',
-      verificationToken: null,
-      emailVerified: true,
-    }, {
-      email: 'jose@yopmail.com',
-      password: 'password',
-      verificationToken: null,
-      emailVerified: true,
-    }, {
-      email: 'unverified@yopmail.com',
-      password: 'password',
-      verificationToken: null,
-      emailVerified: false,
-    }, {
-      email: 'jm@yopmail.com',
-      password: 'password',
-      verificationToken: null,
-      emailVerified: true,
-    }, {
-      email: 'roro@yopmail.com',
-      password: 'password',
-      verificationToken: null,
-      emailVerified: true,
-    }], function(err, users) {
+    app.models.myUser.create(userFixture, function (err, users) {
       if (err) throw err;
-      // create the admin role
-      Role.create({
-        name: 'admin',
-      }, function(err, role) {
-        if (err) throw err;
-        // make admin an admin
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: users[0].id,
-        }, function(err, principal) {
-          if (err) throw err;
-        });
-      });
-
-      // create the member role
-      Role.create({
-        name: 'member',
-      }, function (err, role) {
-        if (err) throw err;
-        // make users[1] as member
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: users[1].id,
-        }, function (err, principal) {
-          if (err) throw err;
-        });
-
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: users[2].id,
-        }, function (err, principal) {
-          if (err) throw err;
-        });
-      });
+      setRole('admin', [users[0]]);
+      users.splice(0, 1); // !splice retourne la valeur supprimé
+      setRole('member', users);
     });
   });
 };
---
+++ server/boot/userFixture.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = [{
+  email: 'admin@yopmail.com',
+  password: 'password',
+  verificationToken: null,
+  emailVerified: true,
+}, {
+  email: 'jose@yopmail.com',
+  password: 'password',
+  verificationToken: null,
+  emailVerified: true,
+}, {
+  email: 'unverified@yopmail.com',
+  password: 'password',
+  verificationToken: null,
+  emailVerified: false,
+}, {
+  email: 'jm@yopmail.com',
+  password: 'password',
+  verificationToken: null,
+  emailVerified: true,
+}, {
+  email: 'roro@yopmail.com',
+  password: 'password',
+  verificationToken: null,
+  emailVerified: true,
+}];
