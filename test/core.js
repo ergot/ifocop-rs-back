0bec7966a019b0f2679e5032a968416c1ebf83f9
ergot <wjose14@hotmail.com>
2017-05-26 09:48:27 +0200

    #7 test pour stocker tout les tokens des users

---
+++ test/core.js
@@ -0,0 +1,46 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+const urlRoot = 'http://localhost:3000';
+let usersLogin = require('../server/boot/userFixture');
+
+
+describe('get token', function () {
+  usersLogin.forEach((user) => {
+    it(`with ${user.email}`, function (done) {
+      chai.request(urlRoot)
+        .post('/api/myUsers/login')
+        .send(user)
+        .end(function (err, res) {
+          expect(res).to.satisfy(() => {
+            if (res.statusCode === 200) {
+              expect(res.body.id, 'Pas de token').to.exist;
+              user.token = res.body.id;
+              return true;
+            }
+            if (res.statusCode === 401) {
+              expect(res.body.id).to.be.undefinded;
+              user.token = res.body.id;
+              return true;
+            }
+          });
+          done();
+        });
+    });
+  });
+
+  after(() => {
+    global.CHAI = {};
+    global.CHAI.users = usersLogin;
+    global.CHAI.users.getTokenByEmail = (email) => {
+      for (let i = 0; CHAI.users; i++) {
+        if (CHAI.users[i].email === email) {
+          return CHAI.users[i].token;
+        }
+      }
+    };
+  });
+});
+
