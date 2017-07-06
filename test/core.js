f4bfc021bd9a2e887ed5351d9312cfddfdedc45e
ergot <wjose14@hotmail.com>
2017-07-06 18:52:25 +0200

    #7 supprime la collection friendlist avant de lancer les tests

--- test/core.js
+++ test/core.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* global CHAI */
 'use strict';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -5,14 +7,27 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 const urlRoot = 'http://localhost:3000';
 let usersLogin = require('../server/boot/userFixture');
+const MongoClient = require('mongodb').MongoClient;
+const urlMongo = 'mongodb://localhost/ifocop_RS';
 
-describe('get token', function () {
+describe('Delete collections before test', function() {
+  it('drop friendsLits', function() {
+    MongoClient.connect(urlMongo, function(err, db) {
+      db.collection('friendsList').drop(function(err) {
+        if (err) throw err;
+        db.close();
+      });
+    });
+  });
+});
+
+describe('Get token', function() {
   usersLogin.forEach((user) => {
-    it(`with ${user.email}`, function (done) {
+    it(`with ${user.email}`, function(done) {
       chai.request(urlRoot)
         .post('/api/myUsers/login')
         .send(user)
-        .end(function (err, res) {
+        .end(function(err, res) {
           expect(res).to.satisfy(() => {
             if (res.statusCode === 200) {
               expect(res.body.id, 'Pas de token').to.exist;
@@ -43,4 +58,3 @@ describe('get token', function () {
     };
   });
 });
-
