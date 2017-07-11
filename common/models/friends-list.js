200597a1dd9bca0c40bb561c448cbea611fd3a03
ergot <wjose14@hotmail.com>
2017-07-11 18:38:31 +0200

    #7 ajoute un envoie d email a une friend request

--- common/models/friends-list.js
+++ common/models/friends-list.js
@@ -4,27 +4,39 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost/ifocop_RS';
 
-module.exports = function (Friendslist) {
-  Friendslist.beforeRemote('create', function (ctx, modelInstace, next) {
+module.exports = function(Friendslist) {
+  Friendslist.sendEmail =  function(cb) {
+    Friendslist.app.models.Email.send({
+      to: 'foo@bar.com',
+      from: 'you@gmail.com',
+      subject: 'my subject',
+      text: 'my text',
+      html: 'my <em>html</em>'}, function(err, mail) {
+      cb(err);
+    });
+  };
+
+  Friendslist.beforeRemote('create', function(ctx, modelInstace, next) {
     if (ctx.req.baseUrl === '/api/friendsLists' & ctx.req.method === 'POST') {
       ctx.req.body.sender = ctx.req.accessToken.userId;
 
       const sender = ctx.req.accessToken.userId;
       const receiver = ctx.req.body.receiver;
 
-      MongoClient.connect(url, function (err, db) {
+      let $this = Friendslist;
+
+      MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         db.collection('friendsList').find({
           $or: [
             {sender: sender, receiver: receiver},
-            {sender: receiver, receiver: sender}]
-        }).toArray(function (err2, results) {
+            {sender: receiver, receiver: sender}],
+        }).toArray(function(err2, results) {
           if (err2) throw err;
           db.close();
           if (results.length === 0) {
-            next();
+            $this.sendEmail(next);
           } else {
-            console.log(results)
             const error = new Error('Une friend request est déja en cours');
             error.status = 409;
             return next(error);
