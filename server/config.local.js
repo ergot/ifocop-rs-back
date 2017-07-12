eceffd8ead5431a0520f81b1b1c27aa51d9dc057
ergot <wjose14@hotmail.com>
2017-07-12 15:14:56 +0200

    #7 fichier de config local

---
+++ server/config.local.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * Fichier de config local
+ * @type {{email: {to: string}}}
+ */
+module.exports = {
+  email: {
+    from: 'notification@ifocop-rs.com',
+  },
+  mongo: {
+    url: 'mongodb://localhost/ifocop_RS',
+  },
+};
