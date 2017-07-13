7fbe979958ce5b119c6568f92c9c2867333ae1d4
ergot <wjose14@hotmail.com>
2017-07-13 09:00:18 +0200

    #7 refacto

--- test/friendsList.js
+++ test/friendsList.js
@@ -11,6 +11,29 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost/ifocop_RS';
 
 describe('Ajouter un utilisateur à la liste d’amis', function() {
+  let userRoro = null;
+  before(function(done) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      db.collection('myUser').find({'email': 'roro@yopmail.com'}).toArray(function(error, results) {
+        userRoro = results[0];
+        db.close();
+        done();
+      });
+    });
+  });
+  let userJm = null;
+  before(function(done) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      db.collection('myUser').find({'email': 'jm@yopmail.com'}).toArray(function(error, results) {
+        userJm = results[0];
+        db.close();
+        done();
+      });
+    });
+  });
+
   describe('1. Le membre demandeur clique sur un nom de la liste.', function() {
     it('jm regarde la listes de membres valides', function(done) {
       chai.request(CHAI.urlRoot)
@@ -26,30 +49,7 @@ describe('Ajouter un utilisateur à la liste d’amis', function() {
     });
   });
 
-  describe('2. Le membre choisi, receveur, est ajouté à la liste d’amis du membre', function() {
-    let userRoro = null;
-    before(function(done) {
-      MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        db.collection('myUser').find({'email': 'roro@yopmail.com'}).toArray(function(error, results) {
-          userRoro = results[0];
-          db.close();
-          done();
-        });
-      });
-    });
-    let userJm = null;
-    before(function(done) {
-      MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        db.collection('myUser').find({'email': 'jm@yopmail.com'}).toArray(function(error, results) {
-          userJm = results[0];
-          db.close();
-          done();
-        });
-      });
-    });
-
+  describe('3. Le membre demandeur est ajouté à la liste d’amis du receveur', function() {
     it('jm envoie une friend request à roro', function(done) {
       chai.request(CHAI.urlRoot)
         .post('/api/friendsLists')
@@ -57,18 +57,6 @@ describe('Ajouter un utilisateur à la liste d’amis', function() {
         .send({receiver: userRoro._id})
         .end((err, res) => {
           expect(res).to.have.status(200);
-          done();
-        });
-    });
-
-    it('roro recois un email de notification', function(done){
-      chai.request('https://mailtrap.io/api/v1')
-        .get('/inboxes/214542/messages')
-        .set('Api-Token', parameters.mailtrap['Api-Token'])
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body[0].from_email).to.equal('notification@ifocop-rs.com');
-          expect(res.body[0].to_email).to.equal('roro@yopmail.com');
           done();
         });
     });
@@ -83,7 +71,9 @@ describe('Ajouter un utilisateur à la liste d’amis', function() {
           done();
         });
     });
+  });
 
+  describe('2. Le membre choisi, receveur, est ajouté à la liste d’amis du membre', function() {
     it('roro a une FR de la part de jm', function(done) {
       chai.request(CHAI.urlRoot)
         .get('/api/friendsLists')
@@ -102,6 +92,20 @@ describe('Ajouter un utilisateur à la liste d’amis', function() {
         .send({receiver: userJm._id})
         .end((err, res) => {
           expect(res).to.have.status(409);
+          done();
+        });
+    });
+  });
+
+  describe('4. Un mail est envoyé au membre choisi pour lui signaler l’invitation', function() {
+    it('roro recois un email de notification', function(done) {
+      chai.request('https://mailtrap.io/api/v1')
+        .get('/inboxes/214542/messages')
+        .set('Api-Token', parameters.mailtrap['Api-Token'])
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body[0].from_email).to.equal('notification@ifocop-rs.com');
+          expect(res.body[0].to_email).to.equal('roro@yopmail.com');
           done();
         });
     });
