2e3ffdf8e9b7e08bacd1f368207d6999e0c8b6c1
ergot <wjose14@hotmail.com>
2017-07-26 12:24:08 +0200

    #9 fichier test wall

---
+++ test/wall.js
@@ -0,0 +1,74 @@
+/* eslint-disable max-len,no-unused-expressions */
+/* global CHAI */
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+const libs = require('./libs');
+const parameters = require('../server/parameters');
+
+describe('Publications sur le profil', function() {
+  let users = null;
+  let userJose = null;
+  let userAdmin = null;
+  before('drop friendlist', function(done) {
+    libs.dropCollection(done, 'wall');
+  });
+
+  before('users', function(done) {
+    libs.getUsers(function(data) {
+      users = data;
+      done();
+    });
+  });
+
+  before('user jose', function() {
+    userJose = users.find(function(user) {
+      return user.email === 'jose@yopmail.com';
+    });
+  });
+
+  before('user admin', function() {
+    userAdmin = users.find(function(user) {
+      return user.email === 'jose@yopmail.com';
+    });
+  });
+
+  describe('publier un message', function() {
+
+    it('unverified publie sur le profil de jose', function(done) {
+      chai.request(libs.host.url)
+        .post(`/api/myUsers/${userJose.id}/walls`)
+        .send({message: 'yolo', dateCreated: new Date()})
+        .end((err, res) => {
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+
+    it('jose publie sur son profil', function(done) {
+      console.log(userJose);
+      chai.request(libs.host.url)
+        .post(`/api/myUsers/${userJose.id}/walls`)
+        .set('Authorization', userJose.token)
+        .send({message: 'yolo', dateCreated: new Date()})
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('admin publie sur le profil de jose ', function(done) {
+      console.log(userJose);
+      chai.request(libs.host.url)
+        .post(`/api/myUsers/${userJose.id}/walls`)
+        .set('Authorization', userJose.token)
+        .send({message: 'yolo', dateCreated: new Date()})
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+});
