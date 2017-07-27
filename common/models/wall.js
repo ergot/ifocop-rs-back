4e67989a9d0bccb2abfcf8c2dd88d9222aa8ec34
ergot <wjose14@hotmail.com>
2017-07-27 12:05:55 +0200

    #9 filtre admin member before save

--- common/models/wall.js
+++ common/models/wall.js
@@ -1,5 +1,48 @@
+/* eslint-disable max-len */
 'use strict';
 
 module.exports = function(Wall) {
+  Wall.observe('before save', function(ctx, next) {
+    if (ctx.options.authorizedRoles.admin) {
+      console.log('admin');
+      return next();
+    }
 
+    const myUserId = ctx.instance.myUserId.toString();
+    if (ctx.options.accessToken.userId === myUserId) {
+      console.log('owner');
+      return next();
+    }
+
+    if (ctx.options.authorizedRoles.member) {
+      console.log(ctx.instance);
+
+      const friendId = ctx.instance.friendId;
+
+      // const filtre3 =
+      //   {or: [
+      //     {and: [{sender: friendId}, {receiver: myUserId}]},
+      //     {and: [{sender: myUserId}, {receiver: friendId}]},
+      //   ]};
+
+      Wall.app.models.friendsList.find({where: {isConfirmed: true}}, function(err, users) {
+        for (let i = 0; users[i]; i++) {
+          if (users[i].sender === friendId && users[i].receiver === myUserId) {
+            console.log('for1');
+            return next();
+          }
+          if (users[i].sender === myUserId && users[i].receiver === friendId) {
+            console.log('for2');
+            return next();
+          }
+        }
+        console.log('error');
+        const error = new Error('>>>il faut etre ami afin de publier sur son mur<<<');
+        error.status = 400;
+        return next(error);
+      });
+
+      // next();
+    }
+  });
 };
