a8ba852ac2861ece1128b83081682c4892f27046
ergot <wjose14@hotmail.com>
2017-08-09 12:08:51 +0200

    ajoute firstname et lasnmae dans les fixtures

--- server/boot/userFixture.js
+++ server/boot/userFixture.js
@@ -2,31 +2,43 @@
 module.exports = [{
   email: 'admin@yopmail.com',
   password: 'password',
+  firstName: 'admin',
+  lastName: 'admin',
   verificationToken: null,
   emailVerified: true,
 }, {
   email: 'jose@yopmail.com',
   password: 'password',
+  firstName: 'jose',
+  lastName: 'lucas',
   verificationToken: null,
   emailVerified: true,
 }, {
   email: 'unverified@yopmail.com',
   password: 'password',
+  firstName: 'no',
+  lastName: 'name',
   verificationToken: null,
   emailVerified: false,
 }, {
   email: 'jm@yopmail.com',
   password: 'password',
+  firstName: 'jean michel',
+  lastName: 'robin',
   verificationToken: null,
   emailVerified: true,
 }, {
   email: 'roro@yopmail.com',
   password: 'password',
+  firstName: 'roméo',
+  lastName: 'leclercq',
   verificationToken: null,
   emailVerified: true,
 }, {
   email: 'sam@yopmail.com',
   password: 'password',
+  firstName: 'sam',
+  lastName: 'caron',
   verificationToken: null,
   emailVerified: true,
 }];
