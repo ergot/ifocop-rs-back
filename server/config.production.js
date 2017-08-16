5f5163305c8ca6ec97b285f9dda0e4c1cd39d3c2
ergot <wjose14@hotmail.com>
2017-08-16 17:17:02 +0200

    test url mongo

---
+++ server/config.production.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * Fichier de config local
+ * @type {{email: {to: string}}}
+ */
+module.exports = {
+  email: {
+    from: 'notification@ifocop-rs.com',
+  },
+  mongo: {
+    url: 'mongodb://heroku_x991tj3v:dqfjb2obgrm78640h5f4801uat@ds141450.mlab.com:41450/heroku_x991tj3v',
+  },
+};
