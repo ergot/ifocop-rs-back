87342819ffa43c35dd88a0ca6783e4ffe9f2d05a
ergot <wjose14@hotmail.com>
2017-08-17 16:49:11 +0200

    test host prod email

--- server/config.production.js
+++ server/config.production.js
@@ -10,4 +10,5 @@ module.exports = {
   mongo: {
     url: 'mongodb://heroku_x991tj3v:dqfjb2obgrm78640h5f4801uat@ds141450.mlab.com:41450/heroku_x991tj3v',
   },
+  host: 'tranquil-inlet-21479.herokuapp.com'
 };
